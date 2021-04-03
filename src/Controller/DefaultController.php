c257cda8a81231cb58fdb2acbbb3bd52c9a3e891
Lookas (Łukasz) Konieczny <lookasziebice@gmail.com>
2021-04-03 14:02:15 +0200

    Change path to /api/login_check for API login, create basic authentication method

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="main_page")
+     * @param Request $request
+     * @return Response
+     */
+    public function index(Request $request): Response
+    {
+        return $this->render('base.html.twig');
+    }
+}
