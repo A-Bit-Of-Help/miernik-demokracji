f4649ffe68058ebe0c74a29a400656d1298dc8df
Lookas (Łukasz) Konieczny <lookasziebice@gmail.com>
2021-04-05 13:09:04 +0200

    Added title to timetable.

--- src/Entity/Timetable.php
+++ src/Entity/Timetable.php
@@ -51,6 +51,11 @@ class Timetable
      */
     private $votes;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
     public function __construct()
     {
         $this->votes = new ArrayCollection();
@@ -147,6 +152,18 @@ class Timetable
                 $vote->setTerm(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
 
         return $this;
     }
