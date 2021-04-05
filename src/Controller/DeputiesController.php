6ab8996e30602c42f09566acbf7d8c8ea1d28291
Lookas (Łukasz) Konieczny <lookasziebice@gmail.com>
2021-04-05 20:07:41 +0200

    Added update deputies details if exist. Get link to photo.

--- src/Controller/DeputiesController.php
+++ src/Controller/DeputiesController.php
@@ -17,6 +17,11 @@ use voku\helper\HtmlDomParser;
  */
 class DeputiesController extends AbstractController
 {
+    /**
+     * @param GovernmentPartiesRepository $governmentPartiesRepository
+     * @param DeputiesRepository $deputiesRepository
+     * @Route("/parser", name="deputies_parser", methods={"GET"})
+     */
     public function parserDeputies(GovernmentPartiesRepository $governmentPartiesRepository,
                                    DeputiesRepository $deputiesRepository
     )
@@ -44,24 +49,35 @@ class DeputiesController extends AbstractController
                     $e->findOne('strong')->innerHtml()
                 )
             );
-            foreach($e->find('a') as $a){
+            foreach ($e->find('a') as $a) {
                 $link = 'http://www.sejm.gov.pl' . $a->href;
             };
-
-            $governmentParties = $governmentPartiesRepository->findOneBy(['abbreviation' => $abbreviation]);
-
-            $deputies = new Deputies();
-            $deputies->setFirstname($firstname);
-            $deputies->setMiddlename($middlename);
-            $deputies->setSurname($surname);
-            $deputies->setLink($link);
-            $deputies->setGovernmentParties($governmentParties);
-            if($deputiesRepository->findOneBy(['firstname' => $firstname, 'middlename' => $middlename, 'surname' => $surname]) == null){
-                $em->persist($deputies);
-                $em->flush();
+            $html2 = HtmlDomParser::file_get_html($link);
+            foreach ($html2->find('img') as $e2) {
+                $photo = $e2->src;
             }
 
+            $governmentParties = $governmentPartiesRepository->findOneBy(['abbreviation' => $abbreviation]);
+            if ($deputiesRepository->findOneBy(['firstname' => $firstname, 'middlename' => $middlename, 'surname' => $surname]) == null) {
+                $deputies = new Deputies();
+                $deputies->setFirstname($firstname);
+                $deputies->setMiddlename($middlename);
+                $deputies->setSurname($surname);
+
+            } else {
+                $deputies = $deputiesRepository->findOneBy(['firstname' => $firstname, 'middlename' => $middlename, 'surname' => $surname]);
+            }
+            $deputies->setLink($link);
+            $deputies->setGovernmentParties($governmentParties);
+            $deputies->setPhoto($photo);
+
+            $em->persist($deputies);
+            $em->flush();
         };
+
+        return $this->render('deputies/index.html.twig', [
+            'deputies' => $deputiesRepository->findAll(),
+        ]);
     }
 
     /**
@@ -132,7 +148,7 @@ class DeputiesController extends AbstractController
      */
     public function delete(Request $request, Deputies $deputy): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$deputy->getId(), $request->request->get('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $deputy->getId(), $request->request->get('_token'))) {
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->remove($deputy);
             $entityManager->flush();
