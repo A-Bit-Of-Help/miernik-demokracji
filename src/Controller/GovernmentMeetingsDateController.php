e1605cfe5a8747ee631d4a7b4e8c7e5b6f30a18d
Lookas (Łukasz) Konieczny <lookasziebice@gmail.com>
2021-04-09 09:51:41 +0200

    Added parser for government meetings date, get data from sejm.gov.pl website and save it in database.

--- src/Controller/GovernmentMeetingsDateController.php
+++ src/Controller/GovernmentMeetingsDateController.php
@@ -5,11 +5,15 @@ namespace App\Controller;
 use App\Entity\GovernmentMeetingsDate;
 use App\Form\GovernmentMeetingsDateType;
 use App\Repository\GovernmentMeetingsDateRepository;
+use App\Repository\TimetableRepository;
+use DateTime;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use voku\helper\HtmlDomParser;
 
 /**
  * @Route("/government_meetings_date")
@@ -18,7 +22,51 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 class GovernmentMeetingsDateController extends AbstractController
 {
     /**
+     * @Route("/parser", name="government_meetings_date_parser", methods={"GET"})
+     * @IsGranted("ROLE_ADMIN")
+     * @param GovernmentMeetingsDateRepository $governmentMeetingsDateRepository
+     * @param TimetableRepository $timetableRepository
+     * @return Response
+     * @throws Exception
+     */
+    public function parserGovernmentMeetingsDate(
+        GovernmentMeetingsDateRepository $governmentMeetingsDateRepository,
+        TimetableRepository $timetableRepository
+    ): Response
+    {
+        set_time_limit(0);
+        $em = $this->getDoctrine()->getManager();
+        $votesOnSession = HtmlDomParser::file_get_html('http://sejm.gov.pl/Sejm9.nsf/agent.xsp?symbol=posglos&NrKadencji=9');
+        $elementsOnSession = $votesOnSession->find('.left a');
+        foreach ($elementsOnSession as $eOnSession) {
+            $hrefDate = $eOnSession->href;
+            $votesOnDate = HtmlDomParser::file_get_html('http://sejm.gov.pl/Sejm9.nsf/' . $hrefDate);
+            $string = $votesOnDate->findOne('#title_content h1')->text;
+            $date = new DateTime(
+                str_replace('Głosowania w dniu ', '', substr($string, 0, strpos($string, ' r.')))
+            );
+            $governmentMeeting = $timetableRepository->findOneBy(
+                ['number' => substr($votesOnDate->findOne('#title_content h1')->text, -21, 2)]
+            );
+            if ($governmentMeetingsDateRepository->findOneBy(['GovernmentMeeting' => $governmentMeeting, 'date' => $date]) == null) {
+                $governmentMeetingDate = new GovernmentMeetingsDate();
+                $governmentMeetingDate->setDate($date);
+                $governmentMeetingDate->setGovernmentMeeting($governmentMeeting);
+                $em->persist($governmentMeetingDate);
+            }
+        }
+
+        $em->flush();
+
+        return $this->render('government_meetings_date/index.html.twig', [
+            'government_meetings_dates' => $governmentMeetingsDateRepository->findAll(),
+        ]);
+    }
+
+    /**
      * @Route("/", name="government_meetings_date_index", methods={"GET"})
+     * @param GovernmentMeetingsDateRepository $governmentMeetingsDateRepository
+     * @return Response
      */
     public function index(GovernmentMeetingsDateRepository $governmentMeetingsDateRepository): Response
     {
@@ -30,8 +78,11 @@ class GovernmentMeetingsDateController extends AbstractController
     /**
      * @Route("/new", name="government_meetings_date_new", methods={"GET","POST"})
      * @IsGranted("ROLE_ADMIN")
+     * @param Request $request
+     * @return Response
      */
-    public function new(Request $request): Response
+    public
+    function new(Request $request): Response
     {
         $governmentMeetingsDate = new GovernmentMeetingsDate();
         $form = $this->createForm(GovernmentMeetingsDateType::class, $governmentMeetingsDate);
@@ -53,6 +104,8 @@ class GovernmentMeetingsDateController extends AbstractController
 
     /**
      * @Route("/{id}", name="government_meetings_date_show", methods={"GET"})
+     * @param GovernmentMeetingsDate $governmentMeetingsDate
+     * @return Response
      */
     public function show(GovernmentMeetingsDate $governmentMeetingsDate): Response
     {
@@ -64,6 +117,9 @@ class GovernmentMeetingsDateController extends AbstractController
     /**
      * @Route("/{id}/edit", name="government_meetings_date_edit", methods={"GET","POST"})
      * @IsGranted("ROLE_ADMIN")
+     * @param Request $request
+     * @param GovernmentMeetingsDate $governmentMeetingsDate
+     * @return Response
      */
     public function edit(Request $request, GovernmentMeetingsDate $governmentMeetingsDate): Response
     {
@@ -85,10 +141,13 @@ class GovernmentMeetingsDateController extends AbstractController
     /**
      * @Route("/{id}", name="government_meetings_date_delete", methods={"POST"})
      * @IsGranted("ROLE_ADMIN")
+     * @param Request $request
+     * @param GovernmentMeetingsDate $governmentMeetingsDate
+     * @return Response
      */
     public function delete(Request $request, GovernmentMeetingsDate $governmentMeetingsDate): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$governmentMeetingsDate->getId(), $request->request->get('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $governmentMeetingsDate->getId(), $request->request->get('_token'))) {
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->remove($governmentMeetingsDate);
             $entityManager->flush();
