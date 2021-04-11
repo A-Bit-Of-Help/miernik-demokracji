52b3f5975b30d6e485f704bbe7bd98bdc278ed41
Lookas (Łukasz) Konieczny <lookasziebice@gmail.com>
2021-04-11 15:12:43 +0200

    Added translate to parser index page. Redesigned page view.

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -39,12 +39,12 @@ class DefaultController extends AbstractController
     public function parserSite(Request $request): Response
     {
         $parsers = [
-            ['name' => 'Deputies', 'path' => 'deputies_parser'],
-            ['name' => 'Parties', 'path' => 'government_parties_parser'],
-            ['name' => 'Timetable', 'path' => 'timetable_parser'],
-            ['name' => 'Government meetings date', 'path' => 'government_meetings_date_parser'],
-            ['name' => 'Votes', 'path' => 'votes_parser'],
-            ['name' => 'Votes result', 'path' => 'votes_result_parser'],
+            ['name' => 'Deputies', 'path' => 'deputies_parser', 'list' => 'deputies_index'],
+            ['name' => 'Parties', 'path' => 'government_parties_parser', 'list' => 'government_parties_index'],
+            ['name' => 'Timetable', 'path' => 'timetable_parser', 'list' => 'timetable_index'],
+            ['name' => 'Government meetings date', 'path' => 'government_meetings_date_parser', 'list' => 'government_meetings_date_index'],
+            ['name' => 'Votes', 'path' => 'votes_parser', 'list' => 'votes_index'],
+            ['name' => 'Votes result', 'path' => 'votes_result_parser', 'list' => false],
         ];
         return $this->render('parser/index.html.twig', [
             'parsers' => $parsers
